822aaa790cb34f8be08d55fbdb6037b78c590101
Joseph Zheng <jozheng@unc.edu>
2023-10-28 15:01:04 -0400

    Testing

--- View.py
+++ View.py
@@ -8,11 +8,15 @@ def rgb_convert(red, green, blue):
 class View:
 
 
+
     def __init__(self, controller, count):
+
 
         self.controller = controller
 
+
         self.window = tk.Tk()
+        self.window.resizable(False, False)
         self.window.resizable(False, False)
         self.window.title("sample")
         self.window.geometry("750x500")
@@ -34,6 +38,7 @@ class View:
         self.canvas = tk.Canvas(self.window, background = "white", width = self.width, height = self.height)
 
 
+
     def create_squares(self):
         """Generates squares within cells."""
 
@@ -45,6 +50,7 @@ class View:
                 green = 255 * value
                 blue = 255 * value
                 square = self.canvas.create_rectangle(x_position, y_position, x_position + self.cell_width, y_position + self.cell_width, fill = rgb_convert(red, green, blue))
+
 
 
     def create_grid(self):
