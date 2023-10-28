cb9a032369190dba55bb77d1dcb4e9b4654831a5
ni438cn <97555198+ni438cn@users.noreply.github.com>
2023-10-28 12:33:09 -0400

    next

--- Model.py
+++ Model.py
@@ -10,14 +10,17 @@ class Model:
     def getCell(self, i, j):
         return self.li[i][j]
     def valid(self, i, j):
-        return i > 0 and i < self.width and j > 0  and j < self.height
+        return i >= 0 and i < self.width and j >= 0  and j < self.height
     def alive(self, i, j):
         c = 0
-        for x in range(i-1, 1+2):
+        for x in range(i-1, i+2):
             for y in range(j-1, j+2):
                 if (x != i) or (j!= y):
-                    if self.valid(x, y) and self.li[x][y] == 1:
-                        c += 1
+                    
+                    if self.valid(x, y):
+                        
+                        if self.li[x][y] == 1:
+                            c += 1
         return c
     def cellUpdate(self):
         lin = np.array([[0]*self.height] * self.width)
@@ -26,13 +29,15 @@ class Model:
                 temp = self.alive(x, y)
                 current = self.li[x][y]
                 #cases
+                print(x, y, current, temp)
                 if current == 1 and temp in [2, 3]:
                     lin[x][y] = 1
-                if current ==0  and temp == 3:
+                elif current ==0  and temp == 3:
                     lin[x][y] = 1
                 else:
                     lin[x][y] = 0
-        self.li = lin[:]
+        print(lin)
+        self.li = lin
     def setCell(self, i, j, value):
         self.li[i][j] = value
 
