f0878b02a71520e1c09a8982413840aef2a86dca
ArrynOBrien <iiknineteen19@gmail.com>
2023-10-28 17:20:37 -0400

    add saveConfiguration method for testing

--- GameController.py
+++ GameController.py
@@ -1,8 +1,12 @@
+import numpy as np
+
 class GameController:
     def __init__(self):
 
         # main properties
         self.points = 0
+        self.cell_count = 10
+        self.enemy_configuation = np.array([])
 
     def startGame(self):
         pass
