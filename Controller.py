55ff68eadd4e70786f57f9a37501de17ced957a6
ArrynOBrien <iiknineteen19@gmail.com>
2023-10-28 14:50:13 -0400

    Add start/stop method for controller

--- Controller.py
+++ Controller.py
@@ -6,11 +6,13 @@ import View
 class Controller:
     def __init__(self):
 
-        # propreties
+        # properties
         self.clock = pygame.time.Clock()
-        self.framerate = 1
-        self.cell_count = 25
+        self.framerate = 2
+        self.cell_count = 10
+        self.active = False
 
+        # main modules
         self.model = Model.Model(self.cell_count, self.cell_count)
         self.view = View.View(self, self.cell_count)
 
@@ -21,7 +23,8 @@ class Controller:
         self.model.setCell(6, 7, 1)
         self.model.setCell(7, 6, 1)
 
-        # running update loop
+        # looping
+        self.start()
         while True:
 
             # timing frames per second
@@ -33,12 +36,19 @@ class Controller:
             # updating
             self.update(dt)
 
+    def start(self):
+        self.active = True
+
+    def stop(self):
+        self.active = False
+
     def update(self, dt):
 
         # updating model
-        self.model.cellUpdate()
+        if self.active == True:
+            self.model.cellUpdate()
 
-        # updating view
+        # updating display
         self.view.update()
         self.view.window.update()
 
