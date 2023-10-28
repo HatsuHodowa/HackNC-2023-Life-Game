4208b8de5e4fc4706a14bc9d52e1632570ca870b
Joseph Zheng <jozheng@unc.edu>
2023-10-28 14:58:43 -0400

    Added start button.

--- View.py
+++ View.py
@@ -5,14 +5,25 @@ def rgb_convert(red, green, blue):
     return "#{:02x}{:02x}{:02x}".format(red, green, blue)
 
 
-
 class View:
 
+
     def __init__(self, controller, count):
+
         self.controller = controller
+
         self.window = tk.Tk()
+        self.window.resizable(False, False)
         self.window.title("sample")
         self.window.geometry("750x500")
+
+        font = ('Helvetica', 14, "bold")
+
+        title = tk.Label(self.window, text = "")
+        start = tk.Button(self.window, text = "Start Simulation", font = font, height = 6, width = 15, command = "")
+    
+
+        start.place(x = 555, y = 50)
 
         self.width = 500
         self.height = 500
@@ -22,23 +33,19 @@ class View:
         self.cell_width = int(self.width / self.count)
         self.canvas = tk.Canvas(self.window, background = "white", width = self.width, height = self.height)
 
+
     def create_squares(self):
         """Generates squares within cells."""
-
-
 
         for y, cells in enumerate(self.controller.model.li): 
             for x, value in enumerate(cells):
                 x_position = self.cell_width * x
                 y_position = self.cell_width * y
-                red = int(255 * value)
-                green = int(255 * value)
-                blue = int(255 * value)
+                red = 255 * value
+                green = 255 * value
+                blue = 255 * value
                 square = self.canvas.create_rectangle(x_position, y_position, x_position + self.cell_width, y_position + self.cell_width, fill = rgb_convert(red, green, blue))
 
-        
-                
-                    
 
     def create_grid(self):
         """Creates grids within the UI"""
