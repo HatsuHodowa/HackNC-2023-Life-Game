fa0aad1df5cd428aadc32668d557b0aaaa9b4a1c
Joseph Zheng <jozheng@unc.edu>
2023-10-28 23:55:11 -0400

    Removed scrollbar.

--- ChallengeView.py
+++ ChallengeView.py
@@ -9,15 +9,12 @@ class ChallengeView(SimulationView.SimulationView):
         super().essential_gui(controller, count)
         
         self.canvas.configure(background = "grey")
-        scroll = tk.Scrollbar(self.window, orient = "vertical")
         self.font = ("Helvetica", 30, "bold")
         self.red_score = tk.Label(self.window, font = self.font, text = "0.0", fg = "red", bg = "white")
         self.blue_score = tk.Label(self.window, font = self.font, text = "0.0", fg = "blue", bg = "white")
         self.free_boxes = tk.Label(self.window, font = self.font, text = str(self.controller.current_blocks))
         self.load_level.config(text = "Load Level")
 
-
-        scroll.place(x = 555, y = 400)
         self.red_score.place(x = 570, y = 150)
         self.blue_score.place(x = 630, y = 150)
         self.free_boxes.place(x = 700, y = 200)
