4517364019717ea8fbd30ce768df5341c10596b2
Joseph Zheng <jozheng@unc.edu>
2023-10-28 22:49:53 -0400

    Updated challenge view.

--- ChallengeView.py
+++ ChallengeView.py
@@ -1,12 +1,19 @@
 import SimulationView
+import tkinter as tk
 
 class ChallengeView(SimulationView.SimulationView):
 
 
     def __init__(self, controller, count):
         super().essential_gui(controller, count)
+        self.slider.config(y = 100)
 
-        
+
+        scroll = tk.Scrollbar(self.window)
+
+        scroll.place(x = 555, y = 400)
+
+
 
 
 
