04138f07b0d5f49fb37b5483a8c23ee087a6d12b
Joseph Zheng <jozheng@unc.edu>
2023-10-28 22:20:19 -0400

    Challenge View.

---
+++ ChallengeView.py
@@ -0,0 +1,13 @@
+import SimulationView
+
+class ChallengeView(SimulationView.SimulationView):
+
+
+    def __init__(self, controller, count):
+        super().__init__(self, controller, count)
+
+    
+
+
+            
+        
