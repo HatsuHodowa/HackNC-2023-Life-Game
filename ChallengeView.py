b8e280ee34ce0d5a27ac8227a656fdae5204df5d
Joseph Zheng <jozheng@unc.edu>
2023-10-28 23:04:10 -0400

    Update ChallengeView.py

--- ChallengeView.py
+++ ChallengeView.py
@@ -6,12 +6,22 @@ class ChallengeView(SimulationView.SimulationView):
 
     def __init__(self, controller, count):
         super().essential_gui(controller, count)
-        self.slider.config(y = 100)
-
+        
 
         scroll = tk.Scrollbar(self.window)
+        self.red_score = tk.Label(self.window, font = self.font, text = "0", fg = "red")
+        self.blue_score = tk.Label(self.window, text = "0", fg = "blue")
 
         scroll.place(x = 555, y = 400)
+        self.red_score.place(x = 545, y = 100)
+        self.blue_score.place(x = 560, y = 100)
+
+
+
+    def update_score(self):
+        controller = self.controller
+        self.red_score.config(text = str(controller.opponent_points))
+        self.blue_score.config(text = str(controller.player_points))
 
 
 
