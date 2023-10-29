648d9c973ff6d1cb0653ed5bd69d9c8cc1276f2e
ArrynOBrien <iiknineteen19@gmail.com>
2023-10-28 21:55:43 -0400

    push just in case

---
+++ ChallengeController.py
@@ -0,0 +1,7 @@
+import SimulationController
+
+class ChallengeController(SimulationController.SimulationController):
+    def __init__(self):
+        super().__init__(self)
+
+controller = ChallengeController()
